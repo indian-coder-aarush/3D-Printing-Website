e904e2a1219c31bd21540fc67340d42ef742e437
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-09-28 22:55:37 +0530

    styled the upload button:

--- frontend/src/Order.js
+++ frontend/src/Order.js
@@ -4,7 +4,6 @@ function Order(){
     const [formData, setFormData] = useState({
         Name: "",
         Address: "",
-        ThreeDFile: HTMLUnknownElement
     });
 
     function Button_on_click(){
@@ -23,26 +22,27 @@ function Order(){
     }
 
     let fields = [
-        {id:"Name",label:"Name",value:formData.Name ,type:'text'},
-        {id:"Address",label:"Address",value:formData.Address,type:'text'},
-        {id:"3D File",label:"3D file",value:formData.ThreeDFile,type:'file'}
+        {id:"Name",label:"Name",value:formData.Name ,type:'text',class:'input_field'},
+        {id:"Address",label:"Address",value:formData.Address,type:'text',class:'input_field'}
     ]
     return(
         <div className = "page">
-            <div className = "form-container">
-                {fields.map((field)=>
+            <div className="form-container">
+                {fields.map((field) =>
                     <React.Fragment key={field.id}>
-                        <label htmlFor = {field.id}>{field.label}</label>
-                        <input className = "input_field"
-                               id = {field.id}
+                        <label htmlFor={field.id}>{field.label}</label>
+                        <input className={field.class}
+                               id={field.id}
                                onChange={(event) => Handle_change(event, field.id)}
                                value={field.value}
-                               type = {field.type}/>
+                               type={field.type}/>
                     </React.Fragment>
                 )}
+                <label htmlFor="fileInput" className="tab_buttons">Upload</label>
+                <input type="file" id="fileInput" style={{display : "none"}}/>
             </div>
             <br/>
-            <button className = "tab_buttons_order" onClick = {Button_on_click} >Submit</button>
+            <button className="tab_buttons" onClick = {Button_on_click} >Submit</button>
         </div>
     );
 }
