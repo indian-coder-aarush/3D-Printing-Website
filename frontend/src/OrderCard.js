d32b052ec6dfa4996dfdcf1bd5a0e0db472af456
indian-coder-aarush <aarushrshirpewar@gmail.com>
2025-10-05 19:31:32 +0530

    made the basic order card

--- frontend/src/OrderCard.js
+++ frontend/src/OrderCard.js
@@ -1,7 +1,10 @@
 function OrderCard(props) {
     return (
       <div className="order-card">
-
+          <h1>Name: props.Name</h1>
+          <p>Address to send: props.Address</p>
       </div>
     );
 }
+
+export default OrderCard;
